686657a61a03ee9f1864a6411ce134d9f04a38d8
abenromdhane <abenromdhane@linagora.com>
2016-04-20 00:39:57 +0100

    Challenge

--- client.js
+++ client.js
@@ -4,3 +4,14 @@
 // request documentation is available on https://github.com/request/request
 
 var request = require('request');
+
+request.put({
+  var bike={
+    id:'1',
+    name:'Lapirre',
+    price:'2499',
+    country:'FR'
+  };
+},function(err,res){
+  console.log(err.body);
+});
--- server.js
+++ server.js
@@ -14,8 +14,27 @@ app.get('/', function (req, res) {
   res.send('Welcome to the bikes express challenge, check the README.md for instructions!');
 });
 
-app.get('/bikes', function(req, res) {
+app.get('/bikes', function(req, res, country) {
+  req.query.country
   return res.status(200).json(bikes);
+});
+
+app.get('/bikes/:id', function(req, res) {
+  res.params.id= req.subdomains.id;
+  return res.status(200).json(bikes);
+});
+
+app.delete('/bikes/:id', function(req, res){
+  res.send('the bike is deleted successufelly')
+});
+
+app.put('/bikes/:id', function(req, res){
+  return res.status(201).json(bikes);
+});
+
+app.put('/bikes/:price', function(req, res){
+  req.bike.price = 1.14*req.params.price;
+  return res.status(200).json(req.bike);
 });
 
 app.listen(3000, function () {

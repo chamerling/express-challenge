6b92e0a4f42a78182225936d145393ab16852b82
YoussefRekik <rekik.ysf@gmail.com>
2016-04-23 12:13:25 +0200

    responsesRekikYoussef

--- client.js
+++ client.js
@@ -3,4 +3,23 @@
 // For challenge 4.
 // request documentation is available on https://github.com/request/request
 
+
 var request = require('request');
+request(
+    { method: 'PUT'
+    , uri: 'http://localhost:3000/bikes/newbike'
+    , multipart:
+    [ { 'content-type': 'application/json',  body: JSON.stringify({id: '5', name: 'x', price: '3000', country: 'FR', _attachments: {'message.txt': {follows: true,length: 18, 'content_type': 'text/plain' }}})
+        }
+      , { body: 'I am an attachment' }
+      ]
+    }
+  , function (error, response, body) {
+      if(response.statusCode == 201){
+        console.log('document saved as: http://mikeal.iriscouch.com/testjs/')
+      } else {
+        console.log('error: '+ response.statusCode)
+        console.log(body)
+      }
+    }
+  )
